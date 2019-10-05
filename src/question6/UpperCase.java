2f2abf46c11973acd944254ca3eec664e708ff77
piyua <priyanshi.goyal_cs18@gla.ac.in>
2019-10-05 18:50:00 +0530

    Print the lower case string in upper case letters.

---
+++ src/question6/UpperCase.java
@@ -0,0 +1,11 @@
+package question6;
+
+import java.util.Scanner;
+
+public class UpperCase {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(str.toUpperCase());
+    }
+}
