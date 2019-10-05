105c6c0f5e52782beeef120e6f67243a7821fa48
piyua <priyanshi.goyal_cs18@gla.ac.in>
2019-10-05 18:30:05 +0530

    Took input from User and printed in required format

--- src/question1/Question1.java
+++ src/question1/Question1.java
@@ -1,7 +1,20 @@
 package question1;
 
+import java.util.Scanner;
+
 public class Question1 {
     public static void main(String[] args) {
+        String str;
+        Scanner sc = new Scanner((System.in));
+        str = sc.nextLine();
+        System.out.print("index:\t");
+        for (int i = 0; i < str.length(); i++) {
+            System.out.print(i + "\t");
+        }
+        System.out.print("\nchars:\t");
+        for (int i = 0; i < str.length(); i++) {
+            System.out.print(str.charAt(i) + "\t");
+        }
 
     }
 }
