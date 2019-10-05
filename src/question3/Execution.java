030ca1e984ec284997c0325358949855ad2a23b8
piyua <priyanshi.goyal_cs18@gla.ac.in>
2019-10-05 18:37:51 +0530

    Created execution class for the same

--- src/question3/Execution.java
+++ src/question3/Execution.java
@@ -1,4 +1,13 @@
 package question3;
 
+import java.util.Scanner;
+
 public class Execution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine().trim();
+        VowelChange vw = new VowelChange();
+        vw.Vowel(s);
+
+    }
 }
